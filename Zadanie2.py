39afa806313e8e7491acc368ec83bcff4dfbf144
Bohdanw71459 <w71459@student.wsiz.edu.pl>
2024-11-13 22:46:25 +0100

    Create Zadanie2.py

---
+++ Zadanie2.py
@@ -0,0 +1,5 @@
+print(int(3.0)) # int: 3.0 -> 3
+print(float(3)) # float: 3 -> 3.0
+print(float("3")) # float: "3" -> 3 -> 3.0
+print(str(12.4)) # str: 12.4 -> "12.4"
+print(bool(0)) # bool: if object? -> True else False, 0 -> False, 1 -> True, ...
