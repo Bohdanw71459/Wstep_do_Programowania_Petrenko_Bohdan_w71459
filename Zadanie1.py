a0e3c9b0c130288598718c5a4485da5d0171ff7d
Bohdanw71459 <w71459@student.wsiz.edu.pl>
2024-11-13 22:46:08 +0100

    Create Zadanie1.py

---
+++ Zadanie1.py
@@ -0,0 +1,9 @@
+print(type(1+2)) # int
+print(type(1+4.5)) # float
+print(type(3/2)) # float
+print(type(4/2)) # float
+print(type(3//2)) # int
+print(type(-3 // 2)) # int
+print(type(11 % 2)) # int
+print(type(2 ** 10)) # int
+print(type(8 ** (1/3))) # float
