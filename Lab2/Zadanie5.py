7f11c21fcb978da18135e7bf2345bbe445901b33
Bohdanw71459 <w71459@student.wsiz.edu.pl>
2024-11-27 22:23:12 +0100

    Create Zadanie5.py

---
+++ Lab2/Zadanie5.py
@@ -0,0 +1,10 @@
+tekst = "Rzeszów jest piękny"
+
+# a) Pierwsza litera
+print("a) Pierwsza litera w tekście:", tekst[0])
+
+# b) Siódma, dziesiąta, trzynasta oraz druga litera
+print("Siódma litera:", tekst[6])
+print("Dziesiąta litera:", tekst[9])
+print("Trzynasta litera:", tekst[12])
+print("Druga litera:", tekst[1])
