8b4d3a4f73626ad31fa9c3b8f520866a88c18bfa
Bohdanw71459 <w71459@student.wsiz.edu.pl>
2024-11-27 22:23:39 +0100

    Create Zadanie6.py

---
+++ Lab2/Zadanie6.py
@@ -0,0 +1,19 @@
+tresc = "Python jest super"
+
+# a) Zerowy znak
+print("a) Zerowy znak:", tresc[0])
+
+# b) Ostatni znak
+print("b) Ostatni znak:", tresc[-1])
+
+# c) Co drugi znak, zaczynając od zerowego
+print("c) Co drugi znak, zaczynając od zerowego:", tresc[::2])
+
+# d) Co trzeci znak, zaczynając od pierwszego
+print("d) Co trzeci znak, zaczynając od pierwszego:", tresc[1::3])
+
+# e) Od dziesiątego do ostatniego
+print("e) Od dziesiątego do ostatniego:", tresc[10:])
+
+# f) Od końca do początku
+print("f) Od końca do początku:", tresc[::-1])
